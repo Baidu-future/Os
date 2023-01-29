cb3a39353fd0ed0c7e06841574dd0f2dd6792e2d
v_yuanliangliang <v_yuanliangliang@macdeMacBook-Pro.local>
2023-01-29 14:40:18 +0800

    Initial commit

---
+++ python打开图片.py
@@ -0,0 +1,3 @@
+from PIL import Image
+img=Image.open('/Users/v_yuanliangliang/Desktop/91bec5aff5e8e492a0177d461.png')
+img.show()
---
+++ python打开音乐.py
@@ -0,0 +1,2 @@
+from playsound import  playsound
+playsound('/Users/v_yuanliangliang/Desktop/888.mp3')
